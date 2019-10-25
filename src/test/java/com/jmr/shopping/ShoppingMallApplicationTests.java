44a429ee7358b0f9441aac0d1b56a8631b7a16a2
姜墨然i <1033157312@qq.com>
2019-10-25 22:28:45 +0800

    111

---
+++ src/test/java/com/jmr/shopping/ShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmr.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
