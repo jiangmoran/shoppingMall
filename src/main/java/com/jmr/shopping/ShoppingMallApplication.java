dae76e9ec07ab8770235f3a77b5a2a9a6491fa9d
姜墨然i <1033157312@qq.com>
2019-10-27 13:52:44 +0800

    mybatis整合,后期要改为mybatis-plus

--- src/main/java/com/jmr/shopping/ShoppingMallApplication.java
+++ src/main/java/com/jmr/shopping/ShoppingMallApplication.java
@@ -1,10 +1,22 @@
 package com.jmr.shopping;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class ShoppingMallApplication {
+
+//    @Bean
+//    @ConfigurationProperties(prefix = "db")
+//    public DataSource dateSource() {
+//        DruidDataSource dataSource = new DruidDataSource();
+//        return dataSource;
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingMallApplication.class, args);
