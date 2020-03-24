5de4469c1e02a805f986ab3c6d8e56fa5cc09660
姜墨然i <1033157312@qq.com>
2020-03-24 15:39:07 +0800

    mybatis plus

--- src/main/java/com/jmr/shopping/ShoppingMallApplication.java
+++ src/main/java/com/jmr/shopping/ShoppingMallApplication.java
@@ -1,6 +1,7 @@
 package com.jmr.shopping;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -9,14 +10,15 @@ import org.springframework.context.annotation.Bean;
 import javax.sql.DataSource;
 
 @SpringBootApplication
+@MapperScan("cn.com.vicente.demo.mapper")
 public class ShoppingMallApplication {
 
-    @Bean
-    @ConfigurationProperties(prefix = "db")
-    public DataSource dateSource() {
-        DruidDataSource dataSource = new DruidDataSource();
-        return dataSource;
-    }
+//    @Bean
+//    @ConfigurationProperties(prefix = "db")
+//    public DataSource dateSource() {
+//        DruidDataSource dataSource = new DruidDataSource();
+//        return dataSource;
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingMallApplication.class, args);
